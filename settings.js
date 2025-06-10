19e70c1a69a9575f213b2e5e9c3681d60efff951
ezraogombo <ezra2024w@gmail.com>
2025-06-10 20:51:18 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "Gombwe",
   botOwner: 'Professor', // Your name
   ownerNumber: '254706896334', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
